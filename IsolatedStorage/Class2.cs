e406cd34d24ba9f402b154685f2a79d203f8febb
Sascha <Tika1@gmx.de>
2015-04-28 14:14:13 +0200

    initial commit

---
+++ IsolatedStorage/Class2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using PostSharp.Aspects;
+using PostSharp.Serialization;
+
+namespace IsolatedStorage
+{
+    [PSerializable]
+    public class MethodLogger : OnMethodBoundaryAspect
+    {
+        static String file = "sjh-userFile.txt";
+        static String content = "Hallo";
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            try
+            {
+                var store = IsolatedStorageFile.GetUserStoreForApplication();
+                //store.IncreaseQuotaTo();
+                IsolatedStorageFileStream isfs = store.OpenFile(file, FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(isfs);
+                sw.WriteLine(string.Format("{0}", content));
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            base.OnEntry(args);
+        }
+
+
+        
+    }
+}
---
+++ IsolatedStorage/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+
+namespace IsolatedStorage
+{
+    public partial class MainPage : UserControl
+    {
+
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        [MethodLogger]
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+                    
+         }
+            }
+        }
+    
+
